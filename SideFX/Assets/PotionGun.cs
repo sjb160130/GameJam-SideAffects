743a3ebb409e962b4a80b2ca7a40655a7edcf637
Unknown <cenesbitt3@gmail.com>
2018-04-11 04:08:07 -0500

    potion throw

---
+++ SideFX/Assets/PotionGun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionGun : MonoBehaviour {
+    public bool isShooting= false;
+    public PotionControl potion;
+    
+    public float potionSpeed;
+
+    public float timeBetweenthrow;
+    private float throwCounter;
+
+    public Transform firePoint;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //var go = Instantiate(potion, transform.position, q);
+
+    }
+    public void Shoot()
+    {
+        var pos = Input.mousePosition;
+        pos.z = transform.position.z - Camera.main.transform.position.z;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+
+        var q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+        //Debug.Log("?");
+        var go = Instantiate(potion, transform.position, q);
+       
+    }
+}
